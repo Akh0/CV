afeabbfeb3b25428a02f3bf0a26301bbd2a6a11c
achille <achille.guillon@gmail.com>
2015-03-26 22:54:51 +0100

    directive competenceRank + linkedin + contenu

--- client/app/competenceRank/competenceRank.directive.js
+++ client/app/competenceRank/competenceRank.directive.js
@@ -3,14 +3,12 @@
 angular.module('cv2App')
   .directive('competenceRank', function () {
     return {
-      //template: '<div class="rank"></div>',
       restrict: 'EA',
       compile: function (element, attrs) {
           element.append('<div class="rank"></div>');
 
           var rank = parseInt(attrs.competenceRank) || 1;
-          console.log(rank);
-          //alert('ogogol');
+
           for(var i=1; i<5; i++) {
               element.find('.rank').append('<span class="grade-' + i + ' ' + (rank >= i ? 'true' : 'false') + '"></span>')
           }
